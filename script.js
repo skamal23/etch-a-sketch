73a4b4cb5c017d8cfa21ebf3480055e3c8c547ea
Sayem Kamal <sayemkamal12@gmail.com>
2025-01-13 23:53:26 -0500

    Add reset functionality

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ function createGrid(dim) {
     const cont = document.querySelector(".container");
     const w = cont.getBoundingClientRect().width - 10;
     const boxDim = Math.floor(w/dim);
-    for(let i=0;i<dim**2;i++){
+    
+    // Clear any existing grid before creating a new one
+    cont.innerHTML = '';
+
+    for(let i = 0; i < dim**2; i++){
         const div = document.createElement("div");
         div.classList.add("gridbox");
         div.style.height = `${boxDim}px`;
         div.style.width = `${boxDim}px`;
         cont.appendChild(div);
     }
+
+    // Now that the grid is created, add event listeners
+    const boxes = document.querySelectorAll(".gridbox");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => addColor(box))
+    });
 }
+
 createGrid(dimension);
 
 function addColor(element){
@@ -25,13 +36,31 @@ function promptDimension(){
         dim = 100;
     }
     dimension = dim;
-    createGrid(dimension);
+    resizeGrid(dim);
+    createGrid(dim);  // Recreate the grid with the new dimension
 }
 
-const boxes = document.querySelectorAll(".gridbox");
-boxes.forEach((box) => {
-    box.addEventListener("mouseenter", () => addColor(box))
-})
+function resizeGrid(dim) {
+    const boxes = document.querySelectorAll(".gridbox");
+    const cont = document.querySelector(".container");
+    const w = cont.getBoundingClientRect().width - 10;
+    const boxDim = Math.floor(w / dim);
+    boxes.forEach((box) => {
+        box.style.height = `${boxDim}px`;
+        box.style.width = `${boxDim}px`;
+    });
+}
+
+function resetGrid() {
+    const boxes = document.querySelectorAll(".gridbox");
+    boxes.forEach((box) => {
+        box.classList.remove("colorbox");
+    });
+}
 
 const dimBox = document.querySelector(".limit");
-dimBox.addEventListener("click",() => promptDimension());
+dimBox.addEventListener("click", () => promptDimension());
+
+// Adding reset functionality
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => resetGrid());
